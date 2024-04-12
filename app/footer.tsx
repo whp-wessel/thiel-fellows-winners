8ffeeccae08344963136b2ccb9d38d194ecc4bdd
Wes <aerosol.liege-0b@icloud.com>
2024-04-12 15:59:32 +0200

    updated Footer

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,11 +1,11 @@
 export default function Footer() {
     return (
         <>
-            <p className="mb-4">Project by <a className="underline" href="https://nabeelqu.co">Nabeel S. Qureshi</a>; design inspired by <a className="underline" href="https://thesephist.com">Linus Lee&lsquo;s</a> project, <a className="underline" href="https://ycvibecheck.com/">YC Vibe Check</a>.</p>
+            <p className="mb-4">Project based on <a className="underline" href="https://nabeelqu.co">Nabeel S. Qureshi&apos;</a> <a className="underline" href="https://www.evwinners.org/">Emergent Ventures winners; design inspired by <a className="underline" href="https://thesephist.com">Linus Lee&lsquo;s</a> project, <a className="underline" href="https://ycvibecheck.com/">YC Vibe Check</a>; made by <a className="underline" href="https://twitter.com/whp_wessel">Wes</a>.</p>
             <p className="mb-4">Written in <a className="underline" href="https://nextjs.org/">next.js</a>, with semantic search thanks to <a className="underline" href="https://huggingface.co/docs/transformers.js/index">transformers.js</a> and <a className="underline" href="https://www.sbert.net/">sentence-transformers</a>. The model is <a href="https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2" className="underline">all-MiniLM-L6-v2</a>.</p>
-            <p className="mb-4">Data sourced by Nabeel from <a className="underline" href="https://marginalrevolution.com">Marginal Revolution</a> and starting from <a className="underline" href="https://newscience.org/emergent-ventures-winners/">Alexey Guzey&lsquo;s base.</a></p>
-            <p className="mb-4"><a className="underline" href="https://www.mercatus.org/emergent-ventures">Emergent Ventures</a> lives at the Mercatus Center and was founded by <a className="underline" href="https://en.wikipedia.org/wiki/Tyler_Cowen">Tyler Cowen</a>. This is not an official website.</p>
-            <p className="mb-4">Last updated April 2024, up to cohort #33. Submit a data update to the <a className="underline" href="https://github.com/nqureshi/ev-search-python/tree/main/data">Github repo</a>.</p>
+            <p className="mb-4">Data sourced by Wes from the Foundation&apos;s <a className="underline" href="https://www.businesswire.com/portal/site/home/search/?searchType=all&searchTerm=Thiel%20Foundation%20Announces&searchPage=1">annoucements</a>.</p>
+            <p className="mb-4">The Thiel Fellowship is a program of the <a className="underline" href="https://www.thielfoundation.org/">Thiel Foundations</a> and was founded by <a className="underline" href="https://en.wikipedia.org/wiki/Peter_Thiel">Peter Thiel</a>. This is not an official website.</p>
+            <p className="mb-4">Last updated April 2024, up to Class #13. Submit a data update to the <a className="underline" href="https://github.com/nqureshi/ev-search-python/tree/main/data">Github repo</a>.</p>
         </>
     )
 }
