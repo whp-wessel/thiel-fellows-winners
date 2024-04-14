270a6b7f37ce047bd03f1ae8b0ab4788e3b6d24e
Wes <aerosol.liege-0b@icloud.com>
2024-04-14 13:47:40 +0200

    colour

--- app/container.tsx
+++ app/container.tsx
@@ -61,7 +61,7 @@ export default function Container({ data }: any) {
 
     return(
         <>
-            <div className="bg-[#2596be] p-4 rounded-lg mb-6 text-black">
+            <div className="bg-[#151515] p-4 rounded-lg mb-6 text-black">
                 <SearchBar setLoadingTrue={setLoadingTrue} />
             </div>
             {loading && 
