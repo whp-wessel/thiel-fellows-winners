6106766665a024ee45a1d2ce88e4a3f54d3a5a84
Wes <aerosol.liege-0b@icloud.com>
2024-04-18 11:37:42 +0200

    some changes

--- app/winnersTable.tsx
+++ app/winnersTable.tsx
@@ -1,3 +1,32 @@
+"use client"
+
+import { TableHeader, TableCell, TableRow, TableBody, Table, TableHead } from "@/components/ui/table"
+import {
+    ColumnDef,
+    flexRender,
+    getCoreRowModel,
+    useReactTable,
+    ColumnFiltersState,
+    getFilteredRowModel,
+} from "@tanstack/react-table"
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import { Input } from "@/components/ui/input"
+import { useState } from "react"
+import { usePathname } from "next/navigation"
+
+interface WinnersTableProps<TData, TValue> {
+    columns: ColumnDef<TData, TValue>[]
+    data: TData[]
+    query: string
+}
+
+
 export default function WinnersTable<TData, TValue>({
     columns,
     data,
@@ -5,7 +34,7 @@ export default function WinnersTable<TData, TValue>({
 }: WinnersTableProps<TData, TValue>) {
     const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>(
         []
-    );
+    )
 
     const table = useReactTable({
         data,
@@ -16,15 +45,44 @@ export default function WinnersTable<TData, TValue>({
         state: {
             columnFilters,
         },
-    });
+    })
 
-    // Simplified to only include batches 1 to 13
-    const batchOptions = Array.from({ length: 13 }, (_, i) => (i + 1).toString());
-
+    const getBatchOptions = () => {
+        return Array.from(new Set(data.map((winner: any) => winner.batch)));
+    };
+    
+    const batchOptions = getBatchOptions();
     const pathname = usePathname();
     const handleClearSearch = () => {
         location.replace(`${pathname}?query=`);
     };
+
+// Helper function to check if a string is numeric
+function isNumeric(str: any) {
+    return !isNaN(str) && !isNaN(parseFloat(str));
+}
+
+// For sorting cohorts
+function customSort(a: any, b: any) {
+    const isANumeric = isNumeric(a);
+    const isBNumeric = isNumeric(b);
+
+    if (isANumeric && isBNumeric) {
+        return parseInt(a) - parseInt(b);
+    }
+
+    else if (isANumeric) {
+        return -1;
+    }
+
+    else if (isBNumeric) {
+        return 1;
+    }
+
+    else {
+        return a.localeCompare(b);
+    }
+}
 
     return (
         <>
@@ -42,16 +100,23 @@ export default function WinnersTable<TData, TValue>({
                 <div className="mb-4 w-1/4 mr-8">
                     <Select
                         onValueChange={(value) => {
-                            table.getColumn("batch")?.setFilterValue(value);
+                            if (value === "CLEAR_SELECTION") {
+                                table.getColumn("batch")?.setFilterValue("");
+                            } else {
+                                table.getColumn("batch")?.setFilterValue(value);
+                            }
                         }}
                         defaultValue={(table.getColumn("batch")?.getFilterValue() as string) ?? ""}
                     >
                         <SelectTrigger>
-                            <SelectValue placeholder="Choose class..." />
+                            <SelectValue placeholder="Choose Class..." />
                         </SelectTrigger>
                         <SelectContent>
-                        {batchOptions.map((option) => (
-                            <SelectItem key={option} value={option}>{`Class ${option}`}</SelectItem>
+                        <SelectItem key="clear_option" value="CLEAR_SELECTION">
+                            All Classes
+                        </SelectItem>
+                        {batchOptions.sort(customSort).map((option) => (
+                            <SelectItem key={option} value={option}>{option}</SelectItem>
                         ))}
                         </SelectContent>
                     </Select>
@@ -72,16 +137,18 @@ export default function WinnersTable<TData, TValue>({
                     <TableHeader>
                         {table.getHeaderGroups().map((headerGroup) => (
                             <TableRow key={headerGroup.id}>
-                                {headerGroup.headers.map((header) => (
-                                    <TableHead key={header.id}>
-                                        {header.isPlaceholder
-                                            ? null
-                                            : flexRender(
-                                                header.column.columnDef.header,
-                                                header.getContext()
-                                            )}
-                                    </TableHead>
-                                ))}
+                                {headerGroup.headers.map((header) => {
+                                    return (
+                                        <TableHead key={header.id}>
+                                            {header.isPlaceholder
+                                                ? null
+                                                : flexRender(
+                                                    header.column.columnDef.header,
+                                                    header.getContext()
+                                                )}
+                                        </TableHead>
+                                    )
+                                })}
                             </TableRow>
                         ))}
                     </TableHeader>
@@ -110,5 +177,5 @@ export default function WinnersTable<TData, TValue>({
                 </Table>
             </div>
         </>
-    );
+    )
 }
